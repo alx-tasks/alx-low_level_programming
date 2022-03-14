308c6c3fb1e9acd851043a02d94fefdf1df331d3
Themba <thembangubeni04@gmail.com>
2022-03-14 19:53:37 +0200

    Added positive or negative betty format.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,23 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- * description:
- *    main(void); --> 0
- */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	printf("%ld is positive\n", (long int)n);
 	else if (n == 0)
 	printf("%ld is zero\n", (long int)n);
 	else
 	printf("%ld is negative\n", (long int)n);
-	
 	return (0);
 }
