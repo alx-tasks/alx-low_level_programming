0d4aa15c184c84748cb829b097aa43736d7bed51
Themba <thembangubeni04@gmail.com>
2022-03-22 16:48:45 +0200

    Print array.

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,22 +6,21 @@
  * @a: array int
  * @n: number to print
  */
-
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        if (i == (n - 1))
-        {
-            printf("%d", a[i]);
-        }
-        else
-        {
-            printf("%d, ", a[i]);
-        }
-    }
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
 
 	printf("\n");
 }
