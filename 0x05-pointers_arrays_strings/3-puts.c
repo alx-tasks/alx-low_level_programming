445a79eed77934719ede1f8430a5a7d95ef3afdd
Themba <thembangubeni04@gmail.com>
2022-03-22 13:37:53 +0200

    Puts.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,15 @@
-#include "_putchar.h"
-
 /**
- * _puts - Prints a string followed by a newline.
+ * _puts - prints a string followed by a new line
+ * @str: string it receives as a parameter
  *
- * @str: value string
+ * Return: void
  */
+
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
-
 	_putchar('\n');
 }
