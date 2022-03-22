6b63f70c8e202020de007558191d492803e2b5af
Themba <thembangubeni04@gmail.com>
2022-03-22 15:11:12 +0200

    Puts half.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - prints half a string.
+ *
+ * @str: value string
+ */
+
+void puts_half(char *str)
+{
+	int i, len = strlen(str);
+
+    for (i = len/2; i < len; i++)
+    {
+        putchar(str[i]);
+    }
+}
