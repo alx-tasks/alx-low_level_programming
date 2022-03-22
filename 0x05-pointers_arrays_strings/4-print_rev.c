cbbb551f43c11f28fb56524a325e3e5b0b4e3168
Themba <thembangubeni04@gmail.com>
2022-03-22 14:04:08 +0200

    Print reverse.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,3 @@
-#include <unistd.h>
-
 /**
  * print_rev - prints a string in reverse.
  *
@@ -8,13 +6,12 @@
 
 void print_rev(char *s)
 {
-	char newline = '\n';
 	int i = strlen(s) - 1;
 
 	for (i; i >= 0; i--)
 	{
-		write(1, &(*s + 1), 1);
+		putchar(s[i]);
 	}
 
-	write(1, &newline 1);
+	putchar('\n');
 }
