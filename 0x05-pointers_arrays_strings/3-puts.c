c181a658820bbc9c2b2064a9451d5e17168e7bae
Themba <thembangubeni04@gmail.com>
2022-03-22 13:01:31 +0200

    Puts.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+/**
+ * _puts - Prints a string followed by a newline.
+ *
+ * @str: value string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+
+	_putchar('\n');
+}
