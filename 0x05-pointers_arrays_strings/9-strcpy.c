4a2ae64e0b3a1b75a630e48c77e62255b748d58b
Themba <thembangubeni04@gmail.com>
2022-03-22 16:51:34 +0200

    String copy.

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <string.h>
+
+/**
+ * _strcpy - copies a string from one pointer to another
+ * @dest: pointer to accept string
+ * @src: pointer where string is sourced
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	*(dest + i) = 0;
+
+	return (dest);
+}
