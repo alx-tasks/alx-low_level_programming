dbf67043f3e63d5349b70f881f9efabeb1d62094
Themba <thembangubeni04@gmail.com>
2022-03-22 15:05:27 +0200

    Print reverse.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,9 @@
  */
 void puts2(char *str)
 {
-	int len = strlen(str) - 1, i = 0;
+	int len = strlen(str) - 1, i;
 
-	for (i; i <= len; i += 2)
+	for (i = 0; i <= len; i += 2)
 	{
 		putchar(*(str + i));
 	}
