3c320495f669df5b652ba31b7b1922921dcb85cb
Themba <thembangubeni04@gmail.com>
2022-03-22 13:27:16 +0200

    Puts.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,13 @@
-#include "_putchar.h";
+/**
+ * _putchar - writes the character c to stdout.
+ * 
+ * description:
+ *    print_alphabet(void);
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
 
 /**
  * _puts - Prints a string followed by a newline.
