36c6383013d14301b6f87a659a36768a13035ed0
Themba <thembangubeni04@gmail.com>
2022-03-24 12:04:48 +0200

    String cat.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+/**
+ * *_strcat - Concatenates two strings.
+ *
+ * @dest: char dest pointer
+ * @src: char source of copy
+ *
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, len;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		len++;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+
+	dest[len + i] = '\0';
+
+	return (dest);
+}
