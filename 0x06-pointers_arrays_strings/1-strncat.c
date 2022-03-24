acb32ca9b629a93114c665b13889dadaa80013bd
Themba <thembangubeni04@gmail.com>
2022-03-24 13:41:35 +0200

    strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
  * 
  * @dest: char dest
  * @src: char src
- * @n: int n
+ * @n: n
  *
  * Return: dest
  */
@@ -12,16 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 	int i, dest_len = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
-	{
 		dest_len++;
-	}
+
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[dest_len + i] = src[i];
-	}
-
 	dest[dest_len + i] = '\0';
-
 	return (dest);
 }
