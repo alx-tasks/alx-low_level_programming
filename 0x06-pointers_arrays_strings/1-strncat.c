eab36166d651961a2aac08280c339c56710bc16c
Themba <thembangubeni04@gmail.com>
2022-03-24 13:42:48 +0200

    strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,27 @@
 /**
- * *_strncat - concatenates src to dest.
- * 
- * @dest: char dest
- * @src: char src
- * @n: n
+ * _strncat - concatenates src to dest
+ * @dest: destination address pointed to
+ * @src: source addressed copied from
+ * @n: number of bytes to be concatenated
  *
  * Return: dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, dest_len = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
+	{
 		dest_len++;
-
+	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[dest_len + i] = src[i];
+	}
+
 	dest[dest_len + i] = '\0';
+
 	return (dest);
 }
