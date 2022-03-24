284c0e56902aaf4bf297dcca3c6d28b28b0eb9d5
Themba <thembangubeni04@gmail.com>
2022-03-24 13:34:26 +0200

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+/**
+ * _strcmp - compares two strings using pointers
+ * @s1: the first string pointer
+ * @s2: the second string pointer
+ *
+ * Return: (*s1 - *s2)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 && *s2) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(s1) - *(s2));
+}
