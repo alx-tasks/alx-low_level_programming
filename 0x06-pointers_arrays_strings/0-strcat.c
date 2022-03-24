8386ed2998dacd7bbed8b169ff1a8f0c77faa9c6
Themba <thembangubeni04@gmail.com>
2022-03-24 13:14:32 +0200

    String cat.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,9 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, len = 0;
-
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		len++;
-	}
-
-    for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[len + i] = src[i];
-	}
-
-	dest[strlen(dest)] = '\0';
-
-	return dest;
+	char *save = dest;
+	
+	for (; *dest; ++dest);
+	while (*dest++ = *src++);
+	return(save);
 }
