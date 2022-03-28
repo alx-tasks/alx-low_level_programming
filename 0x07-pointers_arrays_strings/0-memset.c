a4c9f53181932a748a0af8b23bc833530f209fee
Themba <thembangubeni04@gmail.com>
2022-03-28 11:57:05 +0200

    Memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+/**
+ * _memset - Sets the memory address 's' with constant byte 'b'.
+ * 
+ * @s: char s
+ * @b: char b
+ * @n: number bytes to filled
+ *
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
